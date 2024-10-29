baf1aae5bd1bdec0adfdbd4fc12bb3fdcd89c222
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2024-10-29 12:44:27 +0100

    default layout with navbar

---
+++ idea-atlas/layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <NuxtLink to="/">Neco</NuxtLink>
+                <ul>
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li><NuxtLink to="/about">About</NuxtLink></li>
+                    <li><NuxtLink to="/maps">Maps</NuxtLink></li>
+                </ul>
+            </nav>
+        </header>
+        <div>
+        <slot /> <!-- page content -->
+        </div>
+    </div>
+
+</template>
+<style scoped>
+    
+</style>
--- idea-atlas/pages/index.vue
+++ idea-atlas/pages/index.vue
@@ -1,15 +1,5 @@
 <template>
     <div>
-        <header>
-            <nav>
-                <NuxtLink to="/">Neco</NuxtLink>
-                <ul>
-                    <li><NuxtLink to="/">Home</NuxtLink></li>
-                    <li><NuxtLink to="/about">About</NuxtLink></li>
-                    <li><NuxtLink to="/maps">Maps</NuxtLink></li>
-                </ul>
-            </nav>
-        </header>
         <h1>Hello world</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus consequatur obcaecati molestias esse vero temporibus reiciendis tempore modi veritatis animi!</p>
     </div>
