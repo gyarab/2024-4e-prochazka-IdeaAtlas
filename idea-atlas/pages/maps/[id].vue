2e10ceb9e61dc0251a15adfb09de911697a0c33d
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2024-10-29 12:22:44 +0100

    added id which routes id to the html

---
+++ idea-atlas/pages/maps/[id].vue
@@ -0,0 +1,14 @@
+<!-- maps/id jakekoliv id -->
+ <template>
+    <div>
+        <p>Map with id {{ id }}</p>
+    </div>
+ </template>
+ 
+ <script setup lang="ts">
+    const {id} = useRoute().params;
+ </script>
+ 
+ <style scoped>
+ 
+ </style>
