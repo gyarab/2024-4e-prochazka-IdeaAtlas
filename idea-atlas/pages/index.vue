ab7498636d958e2dbd9ba6793f8211e006abeb9a
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2024-10-29 12:15:31 +0100

    edit basic pages

---
+++ idea-atlas/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Hello world</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus consequatur obcaecati molestias esse vero temporibus reiciendis tempore modi veritatis animi!</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ idea-atlas/pages/maps/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>This is default page for maps</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
