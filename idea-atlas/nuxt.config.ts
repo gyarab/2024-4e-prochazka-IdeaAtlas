aaabfbffd7ffbf831744837b25ae6a726511c4d7
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2024-11-03 15:17:12 +0100

    added v-network-graph lib

--- idea-atlas/nuxt.config.ts
+++ idea-atlas/nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  css: ["v-network-graph/lib/style.css"]
 })
--- idea-atlas/pages/maps/[id].vue
+++ idea-atlas/pages/maps/[id].vue
@@ -3,9 +3,12 @@
     <p>This is the default page for map {{ id }}</p>
     <MapGraph />
     <SigmaGraph />
+    <MapNetwork />
   </div>
 </template>
 
 <script setup lang="ts">
+import MapNetwork from '~/components/MapNetwork.vue';
+
 const { id } = useRoute().params;
 </script>
