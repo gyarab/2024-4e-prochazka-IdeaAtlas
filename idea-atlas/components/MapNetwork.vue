49ce6695c5a0e1c23e369d7008b92f64f75a3b68
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2024-11-03 15:37:45 +0100

    removed resize on zoom

--- idea-atlas/components/MapNetwork.vue
+++ idea-atlas/components/MapNetwork.vue
@@ -1,41 +1,51 @@
 <script setup lang="ts">
-  interface Node {
-    name: string;
-  }
 
-  interface Edge {
+import * as vNG from "v-network-graph"
+interface Node {
+    name: string;
+}
+
+interface Edge {
     source: string;
     target: string;
-  }
+}
 
-  const nodes: Record<string, Node> = {};
-  const edges: Record<string, Edge> = {};
+const nodes: Record<string, Node> = {};
+const edges: Record<string, Edge> = {};
 
-  // Generate 50 nodes
-  for (let i = 1; i <= 50; i++) {
+// Generate 50 nodes
+for (let i = 1; i <= 50; i++) {
     nodes[`node${i}`] = { name: `Node ${i}` };
-  }
+}
 
-  // Generate 75 edges
-  for (let i = 1; i <= 75; i++) {
+// Generate 75 edges
+for (let i = 1; i <= 75; i++) {
     const source = `node${Math.ceil(Math.random() * 50)}`;
     const target = `node${Math.ceil(Math.random() * 50)}`;
     edges[`edge${i}`] = { source, target };
-  }
+}
+const configs = reactive(
+    vNG.defineConfigs({
+        view: {
+            scalingObjects: true,
+            minZoomLevel: 0.1,
+            maxZoomLevel: 16,
+        },
+    })
+)
 </script>
 
 <template>
-  <v-network-graph
-    class="graph"
-    :nodes="nodes"
-    :edges="edges"
-  />
+    <v-network-graph class="graph" 
+    :nodes= "nodes" 
+    :edges= "edges"
+    :configs = "configs" />
 </template>
 
 <style>
 .graph {
-  width: 800px;
-  height: 600px;
-  border: 1px solid #000;
+    width: 800px;
+    height: 600px;
+    border: 1px solid #000;
 }
 </style>
