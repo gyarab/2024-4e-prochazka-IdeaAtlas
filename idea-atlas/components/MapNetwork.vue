3e0296933893867625bfcba25407e1f7a9604b6f
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2024-11-03 17:21:44 +0100

    refactor MapNetwork.vue to streamline node and edge generation; update graph styling for full-screen display

--- idea-atlas/components/MapNetwork.vue
+++ idea-atlas/components/MapNetwork.vue
@@ -1,15 +1,13 @@
-
 <script setup lang="ts">
-    
 import * as vNG from "v-network-graph"
 interface Node {
     name: string;
-    }
+}
 
 interface Edge {
     source: string;
     target: string;
-  }
+}
 
 const nodes: Record<string, Node> = {};
 const edges: Record<string, Edge> = {};
@@ -17,43 +15,35 @@ const edges: Record<string, Edge> = {};
 // Generate 50 nodes
 for (let i = 1; i <= 50; i++) {
     nodes[`node${i}`] = { name: `Node ${i}` };
-    }
+}
 
-  // Generate 75 edges
-  for (let i = 1; i <= 75; i++) {
+// Generate 75 edges
+for (let i = 1; i <= 75; i++) {
     const source = `node${Math.ceil(Math.random() * 50)}`;
     const target = `node${Math.ceil(Math.random() * 50)}`;
     edges[`edge${i}`] = { source, target };
-  }
-  // Define the configurations
+}
+
+// Define the configurations
 const configs = reactive(
     vNG.defineConfigs({
-        view: {
-            scalingObjects: true, // Enable scaling objects, so the nodes and edges will be scaled when zooming
-            minZoomLevel: 0.1,
-            maxZoomLevel: 16,
-    },
-  })
-)
+                    view: {
+                        scalingObjects: true, // Enable scaling objects, so the nodes and edges will be scaled when zooming
+                  minZoomLevel: 0.1,
+                  maxZoomLevel: 16,
+        },
+    })
+);
 </script>
 
 <template>
-    <div>
-    <v-network-graph
-    class="graph"
-    
-    :nodes= "nodes"
-    
-    :edges= "edges"
-    :configs = "configs" />
-    </div>
+        <v-network-graph
+            class="fixed inset-0 w-screen h-screen"
+            :nodes="nodes"
+            :edges="edges"
+            :configs="configs"
+        />
 </template>
 
 <style>
-.graph {
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-}
 </style>
