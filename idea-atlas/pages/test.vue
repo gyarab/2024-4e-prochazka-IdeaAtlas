0ce9b101b74ae4b5f34687271f7e2a9e2c98fdd0
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2024-12-27 15:08:12 +0100

    feat: refactor node insertion logic into utils/graphService.js and create test page for node insertion

---
+++ idea-atlas/pages/test.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+import insertNodes from "~/utils/graphService";
+import data from "../utils/data"
+
+const nodes = data.nodes
+
+console.log(nodes)
+
+const supabase = useSupabaseClient();
+
+insertNodes(supabase, nodes);
+
+
+</script>
---
+++ idea-atlas/utils/graphService.js
@@ -0,0 +1,23 @@
+//TODO - hardcoded
+const graph_id = "8bedcacd-0049-4f32-a1e5-4fe72a2080d2";
+
+// Insert nodes into the 'nodes' table in the Supabase database
+//supabse must be passed from somhwere where it is part of nuxt life cycle
+async function insertNodes(supabase,nodes) {
+  // Extract node values and map them to an array of objects for insertion
+  const nodesToInsert = Object.values(nodes).map(node => ({
+    name: node.name,
+    graph_id: graph_id
+  }));
+  const { error } = await supabase
+    .from('nodes')
+    .insert(nodesToInsert);
+  
+  if (error) {
+    console.error('Error inserting nodes:', error);
+  } else {
+    console.log('Nodes inserted successfully');
+  }
+}
+
+export default insertNodes;
