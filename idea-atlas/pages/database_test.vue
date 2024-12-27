87e7b1ae0f46ba83a006d4a6bcf21182cd65b9af
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2024-12-27 14:02:14 +0100

    feat: add database test page with node insertion functionality

---
+++ idea-atlas/pages/database_test.vue
@@ -0,0 +1,22 @@
+
+<template>
+    <div>
+        <h1>Test</h1>
+        <button @click="insertNode">Insert Node</button>
+    </div>
+</template>
+
+<script setup>
+const supabase = useSupabaseClient();
+
+async function insertNode() {
+  const { error } = await supabase
+    .from('test')
+    .insert({ id: 1, name: 'Denmark' });
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('Node inserted successfully');
+  }
+}
+</script>
