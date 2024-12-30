bd45446a77801b1bbc616a3ee79629505a3c1e5e
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2024-12-30 21:16:57 +0100

    feat: add deleteNodes function to manage node deletions in the graph - work in progress

--- idea-atlas/utils/graphManager.js
+++ idea-atlas/utils/graphManager.js
@@ -1,20 +1,38 @@
 // This file contains the functions that are used to manage the graph data structure
-export default { addNewNode };
+export default { addNewNode, deleteNodes };
 
 // adds a new node to the graph
-
 function addNewNode(data, newName, xMousePos, yMousePos) {
     // Find the largest numeric part of the existing keys
     const maxNodeId = findCurrentMaxNodeId(data);
     // Generate the next key
     const nextNodeId = `node${maxNodeId + 1}`;
-    
+
     // Add the new node
     data.nodes[nextNodeId] = { name: newName };
     // Adds the new node to the layouts so its position is tracked
     data.layouts.nodes[nextNodeId] = { x: xMousePos, y: yMousePos };
 
 
+}
+//Deletes multiple nodes from the graph
+function deleteNodes(data, nodesToDelete) {
+
+    const nodes = data.nodes;
+    // Nodes which will remain after deletion 
+    const remainingNodes = {};
+
+    // Spares only the nodes which are not contained in the nodesToDelete Object
+    for (const [key, value] of Object.entries(nodes)) {
+        if (!nodesToDelete.has(key)) {
+            remainingNodes[key] = value;
+        }
+    }
+
+    // Updates the nodes object
+    data.nodes = remainingNodes;
+
+    //TODO - remove edges that are connected to the deleted nodes + layouts
 }
 
 
