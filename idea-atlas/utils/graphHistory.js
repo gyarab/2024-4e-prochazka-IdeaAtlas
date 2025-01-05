eab4785ff11b2b01af3e566093ab98d5a17f7f7c
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2025-01-05 11:40:13 +0100

    preperation for undo button - added history Manager file

---
+++ idea-atlas/utils/graphHistory.js
@@ -0,0 +1,13 @@
+class HistoryManager {
+    constructor() {
+        this.history = [];
+        this.currentIndex = -1;
+        this.maxHistory = 10; // Maximum number of saved states
+    }
+
+
+    // I guess In Js we do not declare function with function keyword in class - strange
+   addToHistory(params) {
+    
+   } 
+}
