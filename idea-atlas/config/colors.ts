30534b728ba81a947e8eda260690867ccca553ba
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2025-02-21 20:54:39 +0100

    added colors into their own new file but not usde out of it yet

---
+++ idea-atlas/config/colors.ts
@@ -0,0 +1,14 @@
+export const colorPalette = [
+    '#3498db', // Blue
+    '#e74c3c', // Red
+    '#2ecc71', // Green
+    '#f1c40f', // Yellow
+    '#9b59b6', // Purple
+    '#e67e22', // Orange
+    '#1abc9c', // Turquoise
+    '#34495e', // Dark Blue
+    '#7f8c8d', // Gray
+    '#2c3e50', // Navy Blue
+] as const;
+
+export const defaultColor = colorPalette[0];
