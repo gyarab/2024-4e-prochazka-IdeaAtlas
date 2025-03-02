ed768cb62c5a4d722d5b3d006d16ac91baf7ed12
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2025-03-02 15:24:13 +0100

    added comments

--- idea-atlas/config/constants.ts
+++ idea-atlas/config/constants.ts
@@ -1,9 +1,14 @@
+// Constants used in the application.
+
+// Constants for creating a new map
 export const MAP_NAME_MAX_LENGTH = 50;
 export const MAP_NAME_MIN_LENGTH = 3;
 export const MAP_DESCRIPTION_MAX_LENGTH = 200;
 
+// Nodes constants
 export const NODE_DEFAULT_SIZE = 20;
 export const NODE_MIN_SIZE = 10;
 export const NODE_MAX_SIZE = 100;
 
+// Edges constants
 export const EDGE_DEFAULT_WIDTH = 2;
