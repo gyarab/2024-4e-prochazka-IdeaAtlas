592a81b122b29e612cd49759745a2d197d6cd176
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2025-03-08 10:04:14 +0100

    added label node config

--- idea-atlas/config/mapNetworkConfig.ts
+++ idea-atlas/config/mapNetworkConfig.ts
@@ -1,55 +1,73 @@
 import * as vNG from "v-network-graph";
 
 export const mainConfig = reactive(
-    vNG.defineConfigs({
-      view: {
-        boxSelectionEnabled: true,
-        selection: {
-          box: {
-            color: "#0000ff20",
-            strokeWidth: 1,
-            strokeColor: "#aaaaff",
-            strokeDasharray: "0",
-          },
-        },
-        grid: {
-          visible: true,
-          interval: 10,
-          thickIncrements: 5,
-          line: {
-            color: "#e0e0e0",
-            width: 1,
-            dasharray: 1,
-          },
-          thick: {
-            color: "#cccccc",
-            width: 1,
-            dasharray: 0,
-          },
-        },
-        layoutHandler: new vNG.GridLayout({ grid: 10 }),
-        scalingObjects: true,
-        minZoomLevel: 0.1,
-        maxZoomLevel: 16,
-      },
-      node: {
-        normal: {
-          type: "circle",
-          radius: node => node.size, // Use the value of each node object
-          color: node => node.color,
-        },
-        hover: {
-          radius: node => node.size + 4,
-          color: node => node.color,
-        },
-        selectable: true,
-      },
-      edge: {
-        selectable: true,
-        normal: {
-          width: edge => edge.width, // Use the value of each edge object
-          color: edge => edge.color,
+  vNG.defineConfigs({
+    view: {
+      boxSelectionEnabled: true,
+      selection: {
+        box: {
+          color: "#0000ff20",
+          strokeWidth: 1,
+          strokeColor: "#aaaaff",
+          strokeDasharray: "0",
         },
       },
-    })
-  );
+      grid: {
+        visible: true,
+        interval: 10,
+        thickIncrements: 5,
+        line: {
+          color: "#e0e0e0",
+          width: 1,
+          dasharray: 1,
+        },
+        thick: {
+          color: "#cccccc",
+          width: 1,
+          dasharray: 0,
+        },
+      },
+      layoutHandler: new vNG.GridLayout({ grid: 10 }),
+      scalingObjects: true,
+      minZoomLevel: 0.1,
+      maxZoomLevel: 16,
+    },
+    node: {
+      normal: {
+        type: "circle",
+        radius: node => node.size, // Use the value of each node object
+        color: node => node.color,
+      },
+      hover: {
+        radius: node => node.size + 4,
+        color: node => node.color,
+      },
+      selectable: true,
+      label: {
+        visible: true,
+        fontFamily: undefined,
+        fontSize: 25,
+        lineHeight: 1.1,
+        color: "#000000",
+        margin: 4,
+        direction: "south",
+        background: {
+          visible: false,
+          color: "#ffffff",
+          padding: {
+            vertical: 1,
+            horizontal: 4,
+          },
+          borderRadius: 2,
+        },
+      },
+    },
+    edge: {
+      selectable: true,
+      normal: {
+        width: edge => edge.width, // Use the value of each edge object
+        color: edge => edge.color,
+      },
+    },
+  })
+);
