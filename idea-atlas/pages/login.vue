00b74af0da43a2799f9be46382550100b65d7cad
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2025-03-22 17:19:20 +0100

    added enter shortcut

--- idea-atlas/pages/login.vue
+++ idea-atlas/pages/login.vue
@@ -2,20 +2,22 @@
     <div class="flex items-center justify-center min-h-screen bg-gray-100">
       <div class="bg-white p-8 rounded-lg shadow-md w-96">
         <h2 class="text-2xl font-bold mb-6 text-center">Login</h2>
-        <!-- Email Input -->
-        <div class="mb-4">
-            <label for="email" class="block text-gray-700">Email: </label>
-            <input class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" id="email" v-model="email" type="email" placeholder="Enter your email" />
-        </div>
-        
-        <!-- Password Input -->
-        <div class="mb-4">
-            <label for="password" class="block text-gray-700">Password: </label>
-            <input class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" id="password" v-model="password" type="password" placeholder="Enter your password" />
-        </div>
-        
-        <!-- Submit Button -->
-        <button @click="signIn" class="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600">Login</button>
+        <form @submit.prevent="signIn">
+          <!-- Email Input -->
+          <div class="mb-4">
+              <label for="email" class="block text-gray-700">Email: </label>
+              <input class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" id="email" v-model="email" type="email" placeholder="Enter your email" required />
+          </div>
+          
+          <!-- Password Input -->
+          <div class="mb-4">
+              <label for="password" class="block text-gray-700">Password: </label>
+              <input class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" id="password" v-model="password" type="password" placeholder="Enter your password" required />
+          </div>
+          
+          <!-- Submit Button -->
+          <button type="submit" class="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600">Login</button>
+        </form>
         
         <!-- Message Container -->
         <div class="mt-4">
