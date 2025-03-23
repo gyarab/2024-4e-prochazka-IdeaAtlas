a3c7170cff94b58705c1d50ef152259efe7c7f59
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2025-03-23 18:21:47 +0100

    zoom bug fix

--- idea-atlas/components/MapNetwork.vue
+++ idea-atlas/components/MapNetwork.vue
@@ -201,6 +201,7 @@ let mousePosition = { x: 0, y: 0 };
 onMounted(async () => {
   if (configs.view) {
   configs.view.layoutHandler = GridConfig;
+  configs.view.mouseWheelZoomEnabled = true;
 }
 
   // Create named handler functions that can be referenced for removal
